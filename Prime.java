c6d6f29552d074b0b27d77e2402bc015e510a065
koteshwar111 <22h51a0596@cmrcet.ac.in>
2024-12-08 19:40:54 +0530

    second

--- Prime.java
+++ Prime.java
@@ -11,9 +11,7 @@ public class Prime {
                 }
             }
 
-            if (count == 2) {
-                System.out.println(num);
-            }
+            
         }
     }
     
