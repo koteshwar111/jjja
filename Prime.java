d00edbc6a3760e40af1a8878979da764ca19c839
koteshwar111 <22h51a0596@cmrcet.ac.in>
2024-12-08 19:37:45 +0530

    first

---
+++ Prime.java
@@ -0,0 +1,20 @@
+public class Prime {
+    public static void main(String[] args) {
+        int upperLimit = 100;
+        
+        for (int num = 2; num <= upperLimit; num++) {
+            int count = 0;
+            
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                System.out.println(num);
+            }
+        }
+    }
+    
+}
